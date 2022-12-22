0ac96f442d1fdb7bd73fba36d2ffad4517e5e173
icepjli1234 <icepjli1234@gmail.com>
2022-12-22 15:17:13 +0800

    clone file

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 # YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
 Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
+#hahahahahahaha 
 
 Usage - sources:
     $ python detect.py --weights yolov5s.pt --source 0                               # webcam
